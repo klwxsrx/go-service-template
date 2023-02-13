0ecfe541821806751f126fdf6a8dd15db1a9dafd
Viacheslav Bartsev <klwxsrx@icloud.com>
2023-02-13 20:52:56 +0700

    Удалил ненужную функцию

--- pkg/sig/term.go
+++ pkg/sig/term.go
@@ -6,10 +6,6 @@ import (
 	"syscall"
 )
 
-func Wait(termSignalsChan <-chan os.Signal) {
-	<-termSignalsChan
-}
-
 func TermSignals() <-chan os.Signal {
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT)
