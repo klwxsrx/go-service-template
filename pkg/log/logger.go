35373ab9964cf0ee150fb492668858cbaaff14be
Viacheslav Bartsev <klwxsrx@icloud.com>
2024-10-05 11:59:36 +0700

    Упростил реализацию логгирования

--- pkg/log/logger.go
+++ pkg/log/logger.go
@@ -7,14 +7,6 @@ import (
 	"os"
 )
 
-type contextKey int
-
-const (
-	fieldsContextKey contextKey = iota
-)
-
-type Level int
-
 const (
 	LevelDisabled Level = iota
 	LevelDebug
@@ -23,6 +15,27 @@ const (
 	LevelError
 )
 
+type (
+	Logger interface {
+		With(fields Fields) Logger
+		WithField(name string, value any) Logger
+		WithError(err error) Logger
+		WithContext(ctx context.Context, fields Fields) context.Context
+		Log(ctx context.Context, lvl Level, msg string)
+		Debug(ctx context.Context, msg string)
+		Info(ctx context.Context, msg string)
+		Warn(ctx context.Context, msg string)
+		Error(ctx context.Context, msg string)
+	}
+
+	Fields map[string]any
+	Level  int
+
+	contextKey int
+)
+
+const fieldsContextKey contextKey = iota
+
 var slogLevelMap = map[Level]slog.Level{
 	LevelDebug: slog.LevelDebug,
 	LevelInfo:  slog.LevelInfo,
@@ -30,23 +43,8 @@ var slogLevelMap = map[Level]slog.Level{
 	LevelError: slog.LevelError,
 }
 
-type Fields map[string]any
-
-type Logger interface {
-	With(fields Fields) Logger
-	WithField(name string, value any) Logger
-	WithError(err error) Logger
-	WithContext(ctx context.Context, fields Fields) context.Context
-	Log(ctx context.Context, lvl Level, msg string)
-	Debug(ctx context.Context, msg string)
-	Info(ctx context.Context, msg string)
-	Warn(ctx context.Context, msg string)
-	Error(ctx context.Context, msg string)
-}
-
 type logger struct {
-	impl   *slog.Logger
-	fields Fields // TODO: delete and simplify
+	impl *slog.Logger
 }
 
 func New(level Level) Logger {
@@ -54,12 +52,12 @@ func New(level Level) Logger {
 		return stub{}
 	}
 
-	return logger{
-		impl: slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-			Level: slogLevelMap[level],
-		})),
-		fields: make(Fields),
-	}
+	impl := slog.New(slog.NewJSONHandler(
+		os.Stdout,
+		&slog.HandlerOptions{Level: slogLevelMap[level]},
+	))
+
+	return logger{impl}
 }
 
 func (l logger) With(fields Fields) Logger {
@@ -67,16 +65,12 @@ func (l logger) With(fields Fields) Logger {
 		return l
 	}
 
-	newFields := deepCopyFields(l.fields, len(fields))
-	mergeFields(newFields, fields)
-	l.fields = newFields
+	l.impl = l.impl.With(convertFields(fields)...)
 	return l
 }
 
 func (l logger) WithField(name string, v any) Logger {
-	newFields := deepCopyFields(l.fields, 1)
-	mergeField(newFields, name, v)
-	l.fields = newFields
+	l.impl = l.impl.With(name, v)
 	return l
 }
 
@@ -94,31 +88,28 @@ func (l logger) WithContext(ctx context.Context, fields Fields) context.Context 
 		return ctx
 	}
 
-	ctxFields, ok := getFieldsFromContext(ctx)
-	if !ok {
-		ctxFields = make(Fields, len(fields))
-	} else {
-		ctxFields = deepCopyFields(ctxFields, len(fields))
-	}
+	ctxFields := getContextFields(ctx)
+	result := make([]any, 0, len(ctxFields)+len(fields)*2)
+	result = append(result, ctxFields...)
+	result = append(result, convertFields(fields)...)
 
-	mergeFields(ctxFields, fields)
-	return context.WithValue(ctx, fieldsContextKey, ctxFields)
+	return setContextFields(ctx, result)
 }
 
 func (l logger) Debug(ctx context.Context, str string) {
-	l.loggerWithFields(ctx).Debug(str)
+	l.withContextFields(ctx).Debug(str)
 }
 
 func (l logger) Info(ctx context.Context, str string) {
-	l.loggerWithFields(ctx).Info(str)
+	l.withContextFields(ctx).Info(str)
 }
 
 func (l logger) Warn(ctx context.Context, str string) {
-	l.loggerWithFields(ctx).Warn(str)
+	l.withContextFields(ctx).Warn(str)
 }
 
 func (l logger) Error(ctx context.Context, str string) {
-	l.loggerWithFields(ctx).Error(str)
+	l.withContextFields(ctx).Error(str)
 }
 
 func (l logger) Log(ctx context.Context, level Level, str string) {
@@ -126,80 +117,35 @@ func (l logger) Log(ctx context.Context, level Level, str string) {
 		return
 	}
 
-	l.loggerWithFields(ctx).Log(ctx, slogLevelMap[level], str)
+	l.withContextFields(ctx).Log(ctx, slogLevelMap[level], str)
 }
 
-func (l logger) loggerWithFields(ctx context.Context) *slog.Logger {
-	fields := l.fields
-	ctxFields := getFieldsFromContextOrNil(ctx)
-	if len(ctxFields) > 0 {
-		fields = deepCopyFields(l.fields, len(ctxFields))
-		mergeFields(fields, ctxFields)
-	}
-	if len(fields) == 0 {
-		return l.impl
-	}
-
-	impl := l.impl
-	for name, value := range fields {
-		impl = impl.With(name, value)
-	}
-	return impl
+func (l logger) withContextFields(ctx context.Context) *slog.Logger {
+	return l.impl.With(getContextFields(ctx)...)
 }
 
-func mergeFields(fields, additional Fields) {
-	for fieldName, fieldValue := range additional {
-		mergeField(fields, fieldName, fieldValue)
-	}
-}
-
-func mergeField(fields Fields, fieldName string, fieldValue any) {
-	existedFieldValue, ok := fields[fieldName]
-	if !ok {
-		fields[fieldName] = fieldValue
-		return
-	}
-
-	existedFieldValueMap, ok := existedFieldValue.(Fields)
-	if !ok {
-		fields[fieldName] = fieldValue
-		return
-	}
-	var fieldValueMap Fields
-	if fieldValueMap, ok = fieldValue.(Fields); !ok {
-		if fieldValueMap, ok = fieldValue.(map[string]any); !ok {
-			fields[fieldName] = fieldValue
-			return
-		}
-	}
-
-	for nestedFieldName, nestedFieldValue := range fieldValueMap {
-		mergeField(existedFieldValueMap, nestedFieldName, nestedFieldValue)
-	}
-	fields[fieldName] = existedFieldValueMap
-}
-
-func getFieldsFromContextOrNil(ctx context.Context) Fields {
-	fields, ok := getFieldsFromContext(ctx)
-	if !ok {
-		return nil
-	}
+func getContextFields(ctx context.Context) []any {
+	fields, _ := ctx.Value(fieldsContextKey).([]any)
 	return fields
 }
 
-func getFieldsFromContext(ctx context.Context) (Fields, bool) {
-	fields, ok := ctx.Value(fieldsContextKey).(Fields)
-	return fields, ok
+func setContextFields(ctx context.Context, fields []any) context.Context {
+	if len(fields) == 0 {
+		return ctx
+	}
+
+	return context.WithValue(ctx, fieldsContextKey, fields)
 }
 
-func deepCopyFields(fields Fields, additionalCapacity int) Fields {
-	newFields := make(Fields, len(fields)+additionalCapacity)
-	for fieldName, fieldValue := range fields {
-		fieldsValue, ok := fieldValue.(Fields)
-		if ok {
-			fieldValue = deepCopyFields(fieldsValue, 0)
-		}
-		newFields[fieldName] = fieldValue
+func convertFields(fields Fields) []any {
+	if len(fields) == 0 {
+		return nil
 	}
-	return newFields
+
+	result := make([]any, 0, len(fields)*2)
+	for key, value := range fields {
+		result = append(result, key, value)
+	}
+
+	return result
 }
